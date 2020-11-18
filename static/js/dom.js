03ecfe1f6ea2dcec26f91e9349d22e1da4e2da6b
Tiborcode <tkelemen33gmail.com>
2020-11-18 14:58:13 +0100

    feat: implement board view with 4 default columns, toggle function to be reset

--- static/js/dom.js
+++ static/js/dom.js
@@ -33,13 +33,13 @@ export let dom = {
     // ***** Boards' list overview *****
 
     createBoardElements: function (board, index) {
-        let boardTitle = `<div class="board-title">${board.title}</div>`;
+        let boardTitle = `<span class="board-title">${board.title}</span>`;
         let headerButtons = `
             <button class="board-add">Add Card</button>
             <button class="board-toggle"><i class="fas fa-chevron-down"></i></button>`;
         const boardHeader = `<div class="board-header" data-board-id="${index+1}">${boardTitle}${headerButtons}</div>`;
-        const boardColumns = `<div class="board-columns">.</div>`;
-        const outerHtml = `<div class="board">${boardHeader}${boardColumns}</div>`;
+        const boardColumns = `<div class="board-columns"></div>`;
+        const outerHtml = `<section class="board">${boardHeader}${boardColumns}</section>`;
         let boardContainer = document.querySelector('.board-container');
         boardContainer.insertAdjacentHTML("beforeend", outerHtml);
     },
@@ -52,6 +52,7 @@ export let dom = {
         let submitButton = document.querySelector('.new-board-title button');
         submitButton.addEventListener('click', () => {
             titleInput.style.display = 'none';
+
         })
     },
 
@@ -101,6 +102,7 @@ export let dom = {
 
     createDefaultColumns: function(event) {
         let clickedToggle = event.currentTarget;
+        //let boardID = clickedToggle.parentElement.dataset.boardId;
         let currentBoard = clickedToggle.parentElement.parentElement.querySelector(".board-columns");
 
             currentBoard.insertAdjacentHTML('afterbegin', `
@@ -121,6 +123,11 @@ export let dom = {
                     <div class="board-column-content"></div>
             </div>
             `)
+         clickedToggle.removeEventListener("click", dom.createDefaultColumns);
+         let currentBoardTitles = clickedToggle.parentElement.children;
+         let currentBoardTitle = currentBoardTitles['0'];
+         currentBoardTitle.removeEventListener("click", dom.changeBoardName);
+         currentBoardTitle.addEventListener('click', dom.closeBoard);
         },
 
     listenForToggleClick: function (){
@@ -129,4 +136,12 @@ export let dom = {
             toggle.addEventListener("click", dom.createDefaultColumns);
         }
     },
+    closeBoard: function (event) {
+            let toBeClosedBoard = event.currentTarget.parentElement.parentElement.querySelector(".board-columns");
+            toBeClosedBoard.style.display = "none";
+            dom.listenForToggleClick();
+            console.log()
+            dom.listenForBoardTitleClick();
+
+    }
 }
